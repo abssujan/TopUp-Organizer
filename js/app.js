df119ffa8461f9501e698d8143ff5fd198533aec
abssujan <abdussabursujan@gmail.com>
2024-04-03 15:04:56 +0600

    create an object

--- js/app.js
+++ js/app.js
@@ -6,3 +6,27 @@ const dropDownRadioField = document.getElementById('dropdownRadio');
 dropDownRadioButton.addEventListener('click', function(){
     dropDownRadioField.classList.toggle('hidden');
 })
+
+// input field
+const phoneInputField = document.getElementById('phone-input');
+const ammountInputField = document.getElementById('ammount-input');
+const optionValueField = document.getElementById('provider')
+const submitBtn = document.getElementById('submit-btn');
+
+// click event at submit btn
+submitBtn.addEventListener('click', function(){
+    const phoneValue = phoneInputField.value;
+    const ammountValue = ammountInputField.value;
+    const optionValue  = optionValueField.value;
+    if(!isNaN(phoneValue) && ammountValue !== ''){
+        
+        let topUpInformation = {
+            number: phoneValue,
+            ammount: ammountValue,
+            providerName: optionValue
+        }
+        console.log(topUpInformation)
+    }else {
+        alert('Please provide a valid number')
+    }
+})
