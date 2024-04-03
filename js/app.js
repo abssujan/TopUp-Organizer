80068c593bd98d59ce313eeec017d9db05eb433d
abssujan <abdussabursujan@gmail.com>
2024-04-03 16:20:11 +0600

    complete browser api and add random ts id

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ const optionValueField = document.getElementById('provider')
 const submitBtn = document.getElementById('submit-btn');
 
 // geeting localstorage list if not found it's give empty arry
-let topUpList = JSON.parse(localStorage.getItem('topUp')) || '[]';
+let topUpList = JSON.parse(localStorage.getItem('topUp')) || [];
 
 // create an arry for random value
 const randomValue = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
@@ -27,19 +27,29 @@ function getRandomValue(){
     const randomTransactionValue = randomValue[randomIndexPosition];
     return randomTransactionValue;
 }
-getRandomValue()
+// converted into  a full length string to show display
+function getRandomString(stringLength){
+    let string = '';
+    for(let i = 0; i < stringLength; i++){
+        string +=  getRandomValue();
+    }
+    return string;
+}
 // click event at submit btn
 submitBtn.addEventListener('click', function(){
     const phoneValue = phoneInputField.value;
     const ammountValue = ammountInputField.value;
     const optionValue  = optionValueField.value;
+    //const randomTransactionFullValue = getRandomString(10);
     if(!isNaN(phoneValue) && ammountValue !== ''){
 
         let topUpInformation = {
             number: phoneValue,
             ammount: ammountValue,
-            providerName: optionValue
+            providerName: optionValue,
+            transactionId: getRandomString(10)
         }
+        console.log(topUpInformation)
         topUpList.push(topUpInformation);
         localStorage.setItem('topUp', JSON.stringify(topUpList));
     }else {
