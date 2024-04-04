a8c1cb6504f80be1e2a7c1ed0e3553cdfc25a48d
abssujan <abdussabursujan@gmail.com>
2024-04-05 00:26:36 +0600

    trying fixed deploy problem

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.html"],
+  content: ["./**/*.html"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-  purge: {
-    enabled: true,
-    content: ['./dist/*.html'],
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
